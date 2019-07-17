2d7646c75e70c37697647197a2799ff2b0d171e3
Sergio Campos <morrodogg8@gmail.com>
2019-07-17 16:59:48 -0700

    Finished Readme in .cpp

--- p3.5/main.cpp
+++ p3.5/main.cpp
@@ -1,15 +1,57 @@
-//
-//  main.cpp
-//  p3.5
-//
-//  Created by Sergio campos on 7/17/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ •• P3.5
+ Write a program that reads in three strings and sorts them lexicographically.
+ 
+ Enter three strings: Charlie Able Baker
+ Able
+ Baker
+ Charlie
+ */
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "Enter three strings: ";
+    string first, second, third;
+    cin >> first >> second >> third;
+    float fLength = first.length(), sLength = second.length(), tLength = third.length();
+    
+    if (fLength >= sLength && fLength >= tLength)
+    {
+        if (sLength >= tLength)
+        {
+            cout << third << "\n" << second << "\n" << first << "\n";
+        }
+        else
+        {
+            cout << second << "\n" << third << "\n" << first << "\n";
+        }
+    }
+    else if (sLength >= fLength && sLength >= tLength)
+    {
+        if (fLength >= tLength)
+        {
+            cout << third << "\n" << first << "\n" << second << "\n";
+        }
+        else
+        {
+            cout << first << "\n" << third << "\n" << second << "\n";
+
+        }
+    }
+    else
+    {
+        if (fLength >= sLength)
+        {
+            cout << second << "\n" << first << "\n" << third << "\n";
+        }
+        else
+        {
+            cout << first << "\n" << second << "\n" << third << "\n";
+            
+        }
+    }
 }
